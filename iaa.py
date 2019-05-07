23b5d48bfc317623e78ed8dad432798570293747
Dena <icositetrahedron@gmail.com>
2019-05-07 05:04:58 -0400

    skip lines with only partial annotation

--- iaa.py
+++ iaa.py
@@ -1,7 +1,26 @@
+from collections import defaultdict
 from nltk.metrics.agreement import AnnotationTask
 import xml.etree.ElementTree as ET
 
-def xml_to_triples(filename):
+def get_tagged_lines(filenames):
+    id_to_location = {}
+    tagged_lines_count = defaultdict(int)
+
+    for filename in filenames:
+        tagged_lines = set([])
+        main = ET.parse(filename).getroot().find("TemporalDirections")
+        if main is not None:
+            tags = main.find("TAGS")
+            if tags is not None:
+                for tag in filter(lambda t: t.tag != "RELATION", tags):
+                    tagged_lines.add(tag.attrib["span_start_line"])
+                    tagged_lines.add(tag.attrib["span_end_line"])
+        for line in tagged_lines:
+            tagged_lines_count[line] += 1
+    return tagged_lines_count
+
+
+def xml_to_triples(filename, tag_filter):
     id_to_location = {}
     description_triples = []
     action_triples = []
@@ -14,7 +33,7 @@ def xml_to_triples(filename):
         tags = main.find("TAGS")
         if tags is not None:
             for tag in sorted(tags, key=lambda t: t.tag):
-                if tag.tag != "RELATION":
+                if tag.tag != "RELATION" and tag_filter(tag):
                     location = "{}.{}~{}.{}".format(tag.attrib["span_start_line"],
                                                     tag.attrib["span_start_char"],
                                                     tag.attrib["span_end_line"],
@@ -40,17 +59,21 @@ def xmls_to_triples(filenames):
     description_triples = []
     action_triples = []
     relation_triples = []
+    tagged_lines = get_tagged_lines(filenames)
+    def tag_filter(tag):
+        return tagged_lines[tag.attrib["span_start_line"]] == 3 and tagged_lines[tag.attrib["span_end_line"]] == 3
     for filename in filenames:
-        triples = xml_to_triples(filename)
+        triples = xml_to_triples(filename, tag_filter)
         description_triples.extend(triples[0])
         action_triples.extend(triples[1])
         relation_triples.extend(triples[2])
     return (description_triples, action_triples, relation_triples)
 
-triples = xmls_to_triples(["xml_by_annotater/emily.xml",
-                           "xml_by_annotater/keren.xml",
-                           "xml_by_annotater/kristen.xml",
-                           "xml_by_annotater/jingdi.xml"])
+filenames = ["xml_by_annotater/keren.xml",
+             "xml_by_annotater/kristen.xml",
+             "xml_by_annotater/jingdi.xml"]
+
+triples = xmls_to_triples(filenames)
 
 print(triples[0])
 task = AnnotationTask(data=triples[0])
